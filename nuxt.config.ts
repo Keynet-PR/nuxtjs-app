6c812632b93cb02165bd8eab206c93c31bc2a080
Phearun RETH <phearureth@keynet-kh.com>
2024-02-20 17:51:00 +0700

    4th

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,13 @@ export default defineNuxtConfig({
     componentDir: './components/ui'
   },
   devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      appUrl: process.env.APP_URL,
+      apiUrl: process.env.API_URL,
+      apiProxyUrl: process.env.API_PROXY_URL,
+    },
+  },
   server: {
     host: "0.0.0.0",
     port: process.env.APP_PORT || 3000,
