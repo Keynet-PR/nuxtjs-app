ef5b6a579fc7c493a8fcd2afebfb195bd28478c5
Phearun RETH <phearureth@keynet-kh.com>
2024-02-20 17:16:38 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+ 
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/i18n",
+  ],
+  shadcn: {
+  
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  devtools: { enabled: true },
+})
