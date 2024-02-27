8ba4e598e2c5d42e8ad43c2c2c9a68cf35fef90c
John Doe <phearureth@keynet-kh.com>
2024-02-27 16:57:00 +0700

    add dshaboard page

--- pages/index.vue
+++ pages/index.vue
@@ -1,94 +1,19 @@
 <template>
-  <div>
-    <Button>Click me</Button>
-  </div>
-  <div>
-    <AlertDialog>
-      <AlertDialogTrigger as-child>
-        <Button variant="outline">
-          Show Dialog
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  </div>
-  <div>
-    <Dialog>
-      <DialogTrigger as-child>
-        <Button variant="outline">
-          Edit Profile
-        </Button>
-      </DialogTrigger>
-      <DialogContent class="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <div class="grid gap-4 py-4">
-          <div class="grid grid-cols-4 items-center gap-4">
-            <Label for="name" class="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" class="col-span-3" />
-          </div>
-          <div class="grid grid-cols-4 items-center gap-4">
-            <Label for="username" class="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" class="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">
-            Save changes
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  </div>
-  <!-- <div>
-    <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="sepia">Sepia</option>
-    </select>
-  </div> -->
+  <section class="main-content">
+    <!-- <DashboardStatusCard />
+    <DashboardAnalyticsChart />
+    <DashboardRecentLinks /> -->
+  </section>
+  <!-- Message list-->
+  <!-- <Threads /> -->
 </template>
 
-<script setup>
-
-// const colorMode = useColorMode()
-// console.log(colorMode.preference)
-
+<script lang="ts" setup>
+  import Threads from "@/components/dashboard/threads.vue";
+  definePageMeta({
+    middleware: ["auth"],
+  });
+  useHead({
+    title: "Dashboard",
+  });
 </script>
-
-<style>
-body {
-  background-color: #fff;
-  color: rgba(0,0,0,0.8);
-}
-.dark-mode body {
-  background-color: #091a28;
-  color: #ebf4f1;
-}
-.sepia-mode body {
-  background-color: #f1e7d0;
-  color: #433422;
-}
-</style>
